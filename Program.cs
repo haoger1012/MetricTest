8d450c5561c766d687980db469c8c4a7988dfbc6
Haoger Chen <haoger1012@gmail.com>
2024-01-01 21:22:33 +0800

    Add Metric

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+    });
+
 var app = builder.Build();
+
+app.MapPrometheusScrapingEndpoint();
 
 app.MapGet("/", () => "Hello World!");
 
